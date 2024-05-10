ddc9f929cf48af1640f9cd8dd82f1083969f5c4d
VirTuaLz <38499824+luigi25@users.noreply.github.com>
2024-05-10 19:01:50 +0200

    first versions of IReFunder and ITimeBombOwner interfaces

---
+++ contracts/model/IReFunder.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/EthereansOS/ethereansos-swissknife/contracts/generic/model/ILazyInitCapableElement.sol";
+
+interface IReFunder is ILazyInitCapableElement {
+    function callWithBenefit(address componentAddr, bytes calldata payload, address restReceiver) external payable returns(bytes memory response);
+    function isRefundable(address componentAddr, bytes4 selector) external view returns(bool);
+
+}
--- contracts/model/ITimeBombOwner.sol
+++ contracts/model/ITimeBombOwner.sol
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@ethereans-labs/swissknife/contracts/generic/model/ILazyInitCapableElement.sol";
+import "https://github.com/EthereansOS/ethereansos-swissknife/contracts/generic/model/ILazyInitCapableElement.sol";
 
 interface ITimeBombOwner is ILazyInitCapableElement {
-
+    
     function timeBombOwner() external view returns (address);
 
     function setTimeBombOwner(address newValue) external returns(address oldValue);
 
     function submit(address location, bytes calldata payload, address restReceiver) external payable returns(bytes memory response);
+    
+    function disableComponent() external;
+    
+    function disableComponentOwner() external;
+
+    event TimeBombOwnershipTransferred(address indexed oldTimeBombOwner, address indexed newTimeBombOwner);
+
+    event TimeBombOwnershipExtended(uint256 indexed extendedSeconds,uint256 indexed extentionNumber, uint256 indexed newEndTime);
+
 }
